ef77fbb150e4028b840add071da691631c611b9d
Tuyen Nguyen <tuyen.nguyen.fr@gmail.com>
2023-09-27 12:30:00 +0200

    php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Silene's world</title>
-        <link rel="stylesheet" href="style.css"
+        <title>CV</title>
+        <link rel="stylesheet" href="style-index.css"
     </head>
 
     <body>
@@ -16,10 +16,10 @@
         <div class="container">
             <div class="square1"></div>
             <div class="square2 police">
-                <p>Qui suis-je?</p></div>
+                <p><a href="qui-suis-je.php">Qui suis-je ?</a></p></div>
             <div class="square1"></div>
         </div>
-
+        
         <div class="container">
             <div class="square2 police"> 
                 <p>Mon parcours</p> </div>
---
+++ qui-suis-je.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="style.css">
+ <title>Qui Suis-Je ?</title>
+</head>
+
+<body>
+ <header>
+  <nav>
+   <ul>
+    <li>Qui suis-je ?</li>
+    <li>Mon parcours</li>
+    <li>Mes compétences</li>
+    <li>Mon portfolio</li>
+   </ul>
+  </nav>
+ </header>
+
+</body>
+</html>
