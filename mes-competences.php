a2b3cbd3c162c04e086b5fcdea947afdbd8f27fe
Tuyen Nguyen <tuyen.nguyen.fr@gmail.com>
2023-10-14 23:33:59 +0200

    mes competences 1

--- mes-competences.php
+++ mes-competences.php
@@ -1,11 +1,48 @@
 <?php
-require_once "header.php";
+require_once "header-autres.php";
 ?>
 
-    <main>
+    <main class="main-parcours">
+
+    <section class="section-job">
+        <div class="date">
+            <p>Compétences</p>
+        </div>
+        <div class="company">
+            <p><strong>Informatique</strong></p>
+        </div>
+        <div class="job-description">
+            <ul>
+                <li>PHP</li>
+                <li>MySQL (MariaDB, SQLite)</li>
+                <li>HTML/CSS</li>
+                <li>Javascript, Java, Python (notions)</li>
+                <li>Github</li>
+            </ul>
+        </div>
+    </section>
 
 
+    <section class="section-job">
+        <div class="date">
+            <p>Formation</p>
+        </div>
+        <div class="company">
+            <p><strong>Diplômes/Ecole</strong></p>
+        </div>
+        <div class="job-description">
+            <ul>
+                <li>En cours de formation de développeuse informatique / Ecole Studi (e-learning)</li>
+                <li>EMA (European Management Academy / ESAM Paris</li>
+                <li>Licence LEA (Anglais/Espagnol) / Université de Cergy-Pontoise</li>
+                <li>Baccalauréat série D (Sciences) / Lycée Alfred Kastler (Cergy-Pontoise)</li>
+            </ul>
+        </div>
+    </section>
 
+    <section>
+    <h3>Centres d'intérêts</h3>
+    </section>
     
     </main>
 
